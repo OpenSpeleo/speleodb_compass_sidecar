bcbde69f8a35d8bedb0b1844471da7d7964227a4
Zach Heylmun <zheylmun@gmail.com>
2025-11-25 09:33:19 -0500

    Add machinery and example for less terrible ipc

---
+++ src/error.rs
@@ -0,0 +1,21 @@
+use wasm_bindgen::JsValue;
+
+#[derive(Clone, Eq, PartialEq, Debug, thiserror::Error)]
+pub enum Error {
+    #[error("Command returned Error: {0}")]
+    Command(String),
+    #[error("Failed to parse JSON: {0}")]
+    Serde(String),
+}
+
+impl From<serde_wasm_bindgen::Error> for Error {
+    fn from(e: serde_wasm_bindgen::Error) -> Self {
+        Self::Serde(e.to_string())
+    }
+}
+
+impl From<JsValue> for Error {
+    fn from(e: JsValue) -> Self {
+        Self::Command(format!("{:?}", e))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,58 @@
 mod app;
 mod components;
+mod error;
 mod speleo_db_controller;
 
 use app::App;
-use wasm_bindgen::{JsValue, prelude::wasm_bindgen};
+use error::Error;
+use serde::{Serialize, de::DeserializeOwned};
 
-#[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "core"])]
-    async fn invoke(cmd: &str, args: JsValue) -> JsValue;
+pub(crate) type Result<T> = core::result::Result<T, Error>;
 
-    // invoke without arguments
-    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "core"], js_name = invoke)]
-    async fn invoke_without_args(cmd: &str) -> JsValue;
+/// Call a command in the backend.
+///
+/// # Example
+///
+/// ```rust,no_run
+/// use tauri_api::tauri::invoke;
+///
+/// struct User<'a> {
+///     user: &'a str,
+///     password: &'a str
+/// }
+///
+/// match invoke("login", &User { user: "tauri", password: "poiwe3h4r5ip3yrhtew9ty" }).await {
+///     Ok(response) => {
+///         println!("Login successful: {:?}", response);
+///     }
+///     Err(e) => {
+///         eprintln!("Login failed: {:?}", e);
+///     }
+/// }
+///
+/// ```
+///
+/// @param cmd The command name.
+/// @param args The optional arguments to pass to the command.
+/// @return A promise resolving to or rejecting the backend response.
+#[inline(always)]
+pub async fn invoke<A: Serialize, R: DeserializeOwned>(cmd: &str, args: &A) -> crate::Result<R> {
+    let raw = inner::invoke(cmd, serde_wasm_bindgen::to_value(args)?).await?;
+    serde_wasm_bindgen::from_value(raw).map_err(Into::into)
+}
+
+/// Inner wasm-bindgen bindings.
+mod inner {
+    use wasm_bindgen::prelude::*;
+    #[wasm_bindgen]
+    extern "C" {
+        #[wasm_bindgen(catch,js_namespace = ["window", "__TAURI__", "core"])]
+        pub async fn invoke(cmd: &str, args: JsValue) -> Result<JsValue, JsValue>;
+    }
 }
 
 fn main() {
     console_error_panic_hook::set_once();
+    wasm_logger::init(wasm_logger::Config::default());
     yew::Renderer::<App>::new().render();
 }
