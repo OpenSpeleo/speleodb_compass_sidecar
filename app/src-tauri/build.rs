e740e368aa6979f95aea177f623936752c03f148
Zach <zheylmun@gmail.com>
2026-02-14 14:40:20 -0500

    Make DSN for sentry an environment

--- app/src-tauri/build.rs
+++ app/src-tauri/build.rs
@@ -1,3 +1,5 @@
 fn main() {
+    // Rebuild when the Sentry DSN env var changes so option_env!() picks it up.
+    println!("cargo:rerun-if-env-changed=SENTRY_DSN_SPELEODB_COMPASS");
     tauri_build::build()
 }
--- app/src-tauri/src/main.rs
+++ app/src-tauri/src/main.rs
@@ -2,15 +2,17 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    let _guard = sentry::init((
-        "https://c6263e5a73bd3970d9c90ce1fd45c8ee@o4510194903875584.ingest.us.sentry.io/4510438173048832",
-        sentry::ClientOptions {
-            release: sentry::release_name!(),
-            // Capture user IPs and potentially sensitive headers when using HTTP server integrations
-            // see https://docs.sentry.io/platforms/rust/data-management/data-collected for more info
-            send_default_pii: true,
-            ..Default::default()
-        },
-    ));
+    let _guard = option_env!("SENTRY_DSN_SPELEODB_COMPASS").map(|dsn| {
+        sentry::init((
+            dsn,
+            sentry::ClientOptions {
+                release: sentry::release_name!(),
+                // Capture user IPs and potentially sensitive headers when using HTTP server integrations
+                // see https://docs.sentry.io/platforms/rust/data-management/data-collected for more info
+                send_default_pii: true,
+                ..Default::default()
+            },
+        ))
+    });
     speleodb_compass_sidecar_lib::run()
 }
